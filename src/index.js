7131d8889c0cd113742bc53e7858dc260848315b
nikitusik <nikitacpmi@gmail.com>
2019-09-25 17:57:33 +0400

    feat: add resolve function

--- src/index.js
+++ src/index.js
@@ -39,6 +39,30 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let str="";
+    let dec="";
+    let count=0;
+    for(let i=0; i<expr.length; i++){
+        if(i%10==0 && i!=0 && str!=''){
+            dec+=MORSE_TABLE[str];
+            str="";
+        }
+        if(expr[i]=='1' && expr[i+1]=='1'){
+            str+="-";
+            i++;
+        }
+        else if(expr[i]=='1' && expr[i+1]=='0'){
+            str+=".";
+            i++;
+        }
+        else if(expr[i]=='*' && count==9){
+            dec+=" ";
+            count=0;
+        }
+        else if(expr[i]=='*'){count++;}
+    }
+    dec+=MORSE_TABLE[str];//decode last element
+    return dec;
 }
 
 module.exports = {
